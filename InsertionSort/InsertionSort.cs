684f30b69ffbbe15ae1cded7a6bb68405cfdd72c
Arunsh93 <Arunhosur93@gmail.com>
2021-10-07 18:15:13 +0530

    Insertion Sort Program

---
+++ InsertionSort/InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class InsertionSort 
+    {
+        public static void InsertSort(IComparable[] strArray)
+        {
+            int i, j;
+            for(i = 1; i<strArray.Length; i++)
+            {
+                IComparable value = strArray[i];
+                j = i - 1;
+                while((j>=0) && (strArray[j].CompareTo(value) > 0))
+                {
+                    strArray[j + 1] = strArray[j];
+                    j--;
+                }
+                strArray[j + 1] = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] names = { "Arun", "Jyotiprasad", "Nagu", "Bharath", "Siddu", "Muttu" };
+            InsertSort(names);
+            foreach(var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
