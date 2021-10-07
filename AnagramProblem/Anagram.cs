d86cbe6efaee26aa0a4704ca367e2c2bf69f4802
Arunsh93 <Arunhosur93@gmail.com>
2021-10-07 16:30:42 +0530

    Anagram Problem

---
+++ AnagramProblem/Anagram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AnagramProblem
+{
+    class Anagram
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Race";
+            string str2 = "Care";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+            if(str1 == str2)
+            {
+                Console.WriteLine("Both Strings are Anagram");
+            }
+            else
+            {
+                Console.WriteLine("Both strings are not anagram");
+            }
+        }
+    }
+}
